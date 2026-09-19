a382081ed94e7b522bcacafc83b7d4f15e358753
Dark <329294058+dbezborodov297-hub@users.noreply.github.com>
2026-09-19 13:25:05 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ ADMIN_IDS = [8907438590]
 
 WEBAPP_URL = 'https://darkgram-fkc2.onrender.com/'
 
-TOKEN = os.environ.get('8901361348:AAFt5i0FTeg4Xi6SalfzE3xFCNJzepRg2IA')
+TOKEN = os.environ.get('8901361348:AAFH5WEtT3gJy_rd2TYTbX1nDqCzOpMJ3Kw')
 
 def load_json(filename, default):
     if not os.path.exists(filename):
