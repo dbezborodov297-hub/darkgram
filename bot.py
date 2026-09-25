e7ddf0ec35f1a1eabe8b427d92e0db9171fd72b4
Dark <329294058+dbezborodov297-hub@users.noreply.github.com>
2026-09-25 22:49:29 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from telebot import types
 
 # ==================== НАСТРОЙКИ ====================
-TOKEN = '8514412667:AAEMuG5f6NfuLAOMaHHWpEMDCAI7MqlsasU'
+TOKEN = '8514412667:AAEROrRRLNegCw2Z3caY4ek4rL4dAbQrkEY'
 
 MIN_PLAYERS = 4
 MAX_PLAYERS = 20
